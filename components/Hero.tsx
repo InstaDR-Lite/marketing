0eed952873cf2d91a756dc6a0d3ceaf2e53812cd
rup2701 <rupesh@creativegray.com>
2026-06-02 19:37:28 -0700

    Added a new dhero img.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ export default function Hero() {
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
               <path d="M8 2v12M2 8h12" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
             </svg>
-            Create Your Permanent Room Link
+            Create Your Room Link
           </button>
           <button className={styles.btnSecondary}>
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
@@ -44,7 +44,7 @@ export default function Hero() {
       </div>
 
        <Image
-          src="/dashboard-preview-1.jpg"
+          src="/dashboard-preview.jpg"
           alt="InstaDR-Lite Dashboard"
           width={900}
         height={900}
